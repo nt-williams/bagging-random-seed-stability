9e4342048faba25b10d87f80beadbb3e8229d5be
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-04-27 14:31:26 -0700

    Update nnet sim with bagging rho

--- scripts/nnet_results.R
+++ scripts/nnet_results.R
@@ -13,10 +13,10 @@ setDT(res_nobag)
 epsilon <- seq(0.001, 0.7, length.out = 1e4)
 
 bound <- function(epsilon) {
-    2 * exp(-(320 * epsilon^2) / (4 * (1/4) + (2 / 3) * epsilon))
+    2 * exp(-(320 * epsilon^2) / (4 * (1/4) + (1 - 1/exp(1)) * epsilon))
 }
 
-png("paper/plots/nnet-phase.png", width = 3, height = 2.5, units = "in", res = 600)
+png("plots/nnet-phase.png", width = 3, height = 2.5, units = "in", res = 600)
 par(mar = c(3, 3, 0.5, 0.5), mgp = c(2, 0.6, 0), cex.axis = 0.8, cex.lab = 0.9)
 plot(epsilon, delta(res$pred, epsilon), type = "l", 
      xlab = expression(paste("Tolerance ", epsilon)), 
@@ -24,14 +24,4 @@ plot(epsilon, delta(res$pred, epsilon), type = "l",
      ylim = c(0, 0.7), xlim = c(0, 0.65))
 lines(epsilon, delta(res_nobag$pred, epsilon), col = "red")
 lines(epsilon, bound(epsilon), lty = "dashed")
-# polygon(
-#   x = c(epsilon, rev(epsilon)),
-#   y = c(rep(par("usr")[4], length(epsilon)), rev(bound(epsilon))),
-#   col = rgb(1, 0, 0, 0.15), border = NA
-# )
-# text(
-#   x = 0.5, y = 0.65,
-#   labels = expression(paste("Not (", epsilon, ",", delta, ")-stable")),
-#   cex = 0.8
-# )
 dev.off()
